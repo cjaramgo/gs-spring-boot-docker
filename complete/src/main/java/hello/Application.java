b592df7882ee426adfd62eb2e0790a2fe0bb1516
cjaramgo <43120125+cjaramgo@users.noreply.github.com>
2018-09-11 17:09:04 -0500

    Update Application.java

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
     @RequestMapping("/")
     public String home() {
-        return "Hola Mundo Soy Carlos!";
+        return "Hola Mundo Soy Carlos!!";
     }
 
     public static void main(String[] args) {
